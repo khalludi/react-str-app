d904b96ddfa81150e1052d08772a2609f94d3354
Khalid Ali <khalid.ali@appfolio.com>
2023-02-10 14:46:02 -0800

    Show rate limited error to user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [keyword, setKeyword] = useState("");
 
   function spinWord(e) {
     fetch("http://localhost:4000/")
-      .then(res => res.json())
-      .then(data => setKeyword(data));
+      .then(res => {
+        if(!res.ok) throw new Error(res.status);
+        else return res.json();
+      })
+      .then(data => setKeyword(data))
+      .catch(e => {
+        toast.error('Slow down!', {
+          position: "top-right",
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          });
+      })
     console.log('You clicked submit.');
   }
 
@@ -21,6 +38,7 @@ function App() {
         <h1>Short term is {keyword}</h1>
         <button onClick={spinWord}>Spin</button>
       </div>
+      <ToastContainer />
     </div>
   );
 }

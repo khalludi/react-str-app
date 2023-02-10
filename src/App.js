1412af9e5a631f71560f0f4750b2f91f2f5de527
Khalid Ali <khalludi123@gmail.com>
2023-02-10 12:57:37 -0500

    Pull adjectives from rust server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+
+  function spinWord(e) {
+    fetch("http://localhost:4000/")
+      .then(res => res.json())
+      .then(data => setKeyword(data));
+    console.log('You clicked submit.');
+  }
+
+  useEffect(() => {
+    spinWord("");
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Short term is {keyword}</h1>
+        <button onClick={spinWord}>Spin</button>
+      </div>
     </div>
   );
 }
